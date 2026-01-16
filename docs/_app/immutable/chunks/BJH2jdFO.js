99826e12778ce828dad6f8b94e8f3c91f7bc05ee
Lynn <luke87joseph@gmail.com>
2026-01-16 22:32:26 +0000

    Deploy peerjs working

--- docs/_app/immutable/chunks/BJH2jdFO.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_5m88vp?.base??"/vogel-server",e=globalThis.__sveltekit_5m88vp?.assets??s??"";export{e as a,s as b};
---
+++ docs/_app/immutable/chunks/BO5xnc5C.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_gzdot3?.base??"/vogel-server",e=globalThis.__sveltekit_gzdot3?.assets??s??"";export{e as a,s as b};
